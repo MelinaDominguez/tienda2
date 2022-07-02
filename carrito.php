d9ed86de63a250679ef765b0a952882204629eee
unknown <you@example.com>
2022-07-02 14:45:15 -0500

    Boton eliminar funcionando

---
+++ carrito.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+
+$mensaje="";
+
+if (isset($_POST['btnAccion'])){
+    
+    switch($_POST['btnAccion']){
+
+        case 'Agregar';
+
+            if(is_numeric( openssl_decrypt($_POST['id'],COD,KEY))){
+                $ID=openssl_decrypt($_POST['id'],COD,KEY);
+                $mensaje.="Ok ID correcto".$ID;
+
+            }else{
+                $mensaje.="Upss.... ID Incorrecto".$ID;
+            }
+            if(is_string(openssl_decrypt($_POST['nombre'],COD,KEY))){
+                $NOMBRE=openssl_decrypt($_POST['nombre'],COD,KEY);
+                $mensaje.="Ok el nombre es: ".$NOMBRE;
+            }else{ $mensaje.="Upss.... hay un error con el nombre"; break;}
+
+                if(is_numeric(openssl_decrypt($_POST['cantidad'],COD,KEY))){
+                    $CANTIDAD=openssl_decrypt($_POST['cantidad'],COD,KEY);
+                    $mensaje.="Ok la cantidad es: ".$CANTIDAD;
+                }else{ $mensaje.="Upss.... hay un error con la cantidad"; break;}
+
+                    if(is_numeric(openssl_decrypt($_POST['precio'],COD,KEY))){
+                        $PRECIO=openssl_decrypt($_POST['precio'],COD,KEY);
+                        $mensaje.="Ok el precio es: ".$PRECIO;
+                    }else{ $mensaje.="Upss.... hay un error con el precio"; break;}
+
+        if(!isset($_SESSION['CARRITO'])){
+
+            $producto=array(
+                'ID'=>$ID,
+                'NOMBRE'=>$NOMBRE,
+                'CANTIDAD'=>$CANTIDAD,
+                'PRECIO'=>$PRECIO
+            );
+            $_SESSION['CARRITO'][0]=$producto;
+        
+        }else{
+            $NumeroProductos=count($_SESSION['CARRITO']);
+            $producto=array(
+                'ID'=>$ID,
+                'NOMBRE'=>$NOMBRE,
+                'CANTIDAD'=>$CANTIDAD,
+                'PRECIO'=>$PRECIO,
+            );
+
+            $_SESSION['CARRITO'][$NumeroProductos]=$producto;
+        }
+        $mensaje=print_r($_SESSION,true);
+
+        break;
+        case "Eliminar":
+        if(is_numeric( openssl_decrypt($_POST['id'],COD,KEY))){
+            $ID=openssl_decrypt($_POST['id'],COD,KEY);
+            
+            
+            foreach($_SESSION['CARRITO'] as $indice=>$producto){
+                if($producto['ID']==$ID){
+                    unset($_SESSION['CARRITO'][$indice]);
+                    echo "<script>alert('Elemento eliminado bro...');</script>";
+                }
+            }
+
+        }else{
+            $mensaje.="Upss.... ID Incorrecto".$ID;
+        }
+    break;
+    }
+}
+
+?>
---
+++ templates/cabecera.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>Tienda</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <!--bootstrap 4.1.3-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <!--Navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container">
+            <a class="navbar-brand" href="index.php">Logo de la empresaa</a>
+            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Inicio<span class="visually-hidden">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="mostrarCarrito.php">Carrito(<?php 
+                        echo (empty($_SESSION['CARRITO']))?0:count($_SESSION['CARRITO']);
+                        ?>)</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Acerca de</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownId">
+                            <a class="dropdown-item" href="#">Locacion</a>
+                            <a class="dropdown-item" href="#">Contacto</a>
+                        </div>
+                    </li>
+                </ul>
+                <form class="d-flex my-2 my-lg-0">
+                    <input class="form-control me-sm-2" type="text" placeholder="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+      </div>
+    </nav>
+    <br>
+    <br>
+    <div class="container">
+        <br>
